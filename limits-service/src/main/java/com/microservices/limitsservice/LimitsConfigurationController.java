55d8e0a359f357cd47511ff8dc2db264d7756c11
SeanLim <happylim99@gmail.com>
2019-11-07 14:57:16 +0800

    create spring configuration to use properties file

--- limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import com.microservices.limitsservice.bean.LimitConfiguration;
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration() {
-		return new LimitConfiguration(100, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 
 }
