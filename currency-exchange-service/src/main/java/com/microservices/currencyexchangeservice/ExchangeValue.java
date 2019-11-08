42d38d9492ac2e2c754409e1aa0213afa64c811c
SeanLim <happylim99@gmail.com>
2019-11-08 11:36:50 +0800

    setup dynamic port

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/ExchangeValue.java
@@ -8,6 +8,8 @@ public class ExchangeValue {
 	private String from;
 	private String to;
 	private BigDecimal conversionMultiple;
+	private int port;
+	
 	public ExchangeValue() {
 		super();
 	}
@@ -38,6 +40,12 @@ public class ExchangeValue {
 	}
 	public BigDecimal getConversionMultiple() {
 		return conversionMultiple;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
 	}
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
