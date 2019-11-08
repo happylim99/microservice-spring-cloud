890f4b2f429eef62fabe6a2108368970c950732c
SeanLim <happylim99@gmail.com>
2019-11-08 12:04:47 +0800

    configure jpa

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10001, 'USD', 'MYR', 4, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10002, 'USD', 'SGD', 2, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10003, 'USD', 'AUD', 1.5, 0);
